df52161152abe20c14bd402b5098229eef253263
Azmi1180 <muhammadazmi4456@gmail.com>
2024-08-26 16:57:02 +0700

    fix path

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const sequelize = require('./config/database');
-const authRoutes = require('./routes/authRoutes');
+const authRoutes = require('./routes/auth.route');
 
 dotenv.config();
 
