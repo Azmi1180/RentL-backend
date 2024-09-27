a980d1e65e3acd35969e9e1d3866f9c88e2182da
kresnamukti48 <kresnamukti48@gmail.com>
2024-09-27 20:08:18 +0700

    refactor(response & booking): add response status & booking by login user

--- validations/booking.validation.js
+++ validations/booking.validation.js
@@ -3,7 +3,6 @@ const Joi = require('joi');
 // Validation for creating a new Booking
 exports.validateBooking = (booking) => {
     const schema = Joi.object({
-        user_id: Joi.number().required(),
         lapangan_id: Joi.number().required(),
         booking_date: Joi.date().required(),
         start_time: Joi.string().pattern(/^([01]\d|2[0-3]):00$/).required()
